58c3122d34e0ce88d7f564d11788f4b746f022b8
rikako <jc180215b@gmail.com>
2022-11-23 22:06:37 +0900

    modify:model_html

--- src/main/java/com/example/hello/controller/HelloController.java
+++ src/main/java/com/example/hello/controller/HelloController.java
@@ -1,6 +1,7 @@
 package com.example.hello.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -15,5 +16,12 @@ public class HelloController {
   @GetMapping("/sayHello")
   public String sayHello(){
     return "hello";
+  }
+
+  @GetMapping("/greetModel")
+  public String greetModel(Model model) {
+    String word = "Hello,Rika!（model）";
+    model.addAttribute("message", word);
+    return "greetmodel";
   }
 }
