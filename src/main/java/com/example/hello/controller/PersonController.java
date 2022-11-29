9b39f2e15ad1599ab818885715717d4406e59d12
rikako <jc180215b@gmail.com>
2022-11-29 17:59:06 +0900

    modify:エラーメッセージ表示

--- src/main/java/com/example/hello/controller/PersonController.java
+++ src/main/java/com/example/hello/controller/PersonController.java
@@ -1,22 +1,29 @@
 package com.example.hello.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+
+
+import com.example.hello.models.Person;
 
 @Controller
 public class PersonController {
   
   @GetMapping("/")
-  public String index(){
+  public String index(@ModelAttribute Person person){
     return "person/index";
   }
 
   @PostMapping("/create")
-  public String create(@RequestParam String name, Model model){
-    model.addAttribute("name", name);
+  public String create(@Validated @ModelAttribute Person person, BindingResult result){
+    //バリデーションがある場合はindex.htmlを表示
+    if(result.hasErrors()){
+      return "person/index";
+    }
     return "person/create";
   }
 }
---
+++ src/main/java/com/example/hello/models/Person.java
@@ -0,0 +1,30 @@
+package com.example.hello.models;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Person {
+  @NotBlank
+  @Size(max = 120)
+  private String name;
+
+  @NotNull
+  @Min(0)
+  @Max(120)
+  private Integer age;
+
+  @NotBlank
+  @Email
+  @Size(max = 254)
+  private String email;
+
+}
